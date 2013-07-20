d6f69ae67bfc0a41957081da86107ad97fb7508f
JeremyNixon <jnixon2@gmail.com>
2013-07-20 17:30:05 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Magnificent::Application.config.secret_token = '13bd45afe54a881a3a230196828c32fa81543e03199607dee9fa87ccb81e23764477c9c3bd98e88bcd056d46bc1cba1512ccf1aacf58d79e70d074336435f291'
